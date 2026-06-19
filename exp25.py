fdbeef9f278ba629564cffe235c5a1f4991a6c8b
aishvarya47 <154430479+aishvarya47@users.noreply.github.com>
2026-06-19 22:05:58 +0530

    Add line plotting with Matplotlib in exp25.py

---
+++ exp25.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+
+y1 = [1,2,3,4,5]
+y2 = [2,4,6,8,10]
+
+plt.plot(x,y1,label='Line 1')
+plt.plot(x,y2,label='Line 2')
+
+plt.legend()
+plt.show()
